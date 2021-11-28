1ab37678878d191a2d4c4f19ab18545f3e3b003c
lore <lore@test.com>
2021-11-28 08:56:55 +0200

    Update

---
+++ Data/Tamaian_Rares_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marc_Loredana_Lab8.Models;
+
+namespace Marc_Loredana_Lab8.Data
+{
+    public class Marc_Loredana_Lab8Context : DbContext
+    {
+        public Marc_Loredana_Lab8Context (DbContextOptions<Marc_Loredana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marc_Loredana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Marc_Loredana_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Marc_Loredana_Lab8.Data;
+using Marc_Loredana_Lab8.Models;
+
+namespace Marc_Loredana_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Marc_Loredana_Lab8.Data.Marc_Loredana_Lab8Context _context;
+
+        public IndexModel(Marc_Loredana_Lab8.Data.Marc_Loredana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
